491100e2e0ad9dcfa2b87c87d40932a1d4160226
Baptiste Rouger <rouger.b@gmail.com>
2016-11-04 22:59:57 +0100

    Updated the adding of a hive, removed tracking of pycache directory, working on modification of daughters

--- HiveKeeper.py
+++ HiveKeeper.py
@@ -11,7 +11,7 @@ while entry != "q":
     entry = input("\n==========\nWhat do you want to do ? (Quit : q, Help : h) : ").lower()
     
     if entry == "h":
-        print("Quit (q)\nGet this help (h)\nList the hives (l)\nAdd a hive (a)\n remove (r)\n modify (m)\n or get infos on a hive (i)")
+        print("Quit (q)\nGet this help (h)\nList the hives (l)\nAdd a hive (a)\nModify a hive (m)\n or get infos on a hive (i)")
         
     if entry == "l":
         hkfunctions.hk_list()
@@ -22,4 +22,12 @@ while entry != "q":
         hkfunctions.hk_infos(hiveName)
     if entry == "a":
         hkfunctions.hk_newHive()
- 
+        hiveList = hkfunctions.hk_lstRefresh()
+    if entry == "m":
+        index = int(input("What is the number of the hive that you want to modify ? "))
+        hiveName = hiveList[index]
+        action = int(input("What do you want to modify in this hive : Location (1), Daughter (2)"))
+        if action == 1:
+            hkfunctions.hk_locationModif(hiveName)
+        if action == 2:
+            hkfunctions.hk_daughterModif(hiveName)
--- hkfunctions.py
+++ hkfunctions.py
@@ -14,6 +14,15 @@ def hk_list():
         print(str(i) + ". Hive " + hive[0])
         i += 1
 
+def hk_lstRefresh():
+    """ Function that refreshes the list of the hives stored in the folder
+    """
+    hiveList = []
+    for (dirpath, dirnames, filenames) in os.walk("./Hives"):
+        hiveList.extend(filenames)
+    
+    return hiveList
+
 def hk_infos(hivename):
     """ Function that returns the infos of a hive
     """
@@ -38,4 +47,43 @@ def hk_newHive():
     loc = input("What is the location of the new hive ? ")
     mother = input("What is the name of the mother of this hive ? (If none, write none) ")
     daughter = input("What is the name of the daughter of this hive ? (If none, write none) ")
-    hive.write("\n".join([loc, mother, daughter]))
+    hive.write("\n".join(["*"+loc, "$"+mother, "%"+daughter]))
+    print("\n".join([loc, mother, daughter]))
+    hive.close()
+    
+    hive = open("./Hives/"+mother, "r")
+    hiveMother = hive.readlines()
+    hive.close()
+    
+    hiveMother[2] = ",".join([hiveMother[2], newName])+"\n"
+    hive = open("./Hives/"+mother, "w")
+    print("".join(hiveMother))
+    hive.write("".join(hiveMother))
+    hive.close()
+
+def hk_locationModif(hiveName):
+    """ Function that modifies the location of a specified hive
+    """
+    fileFlux = open("./Hives/"+hiveName, "r")
+    hiveInfos = fileFlux.readlines()
+    fileFlux.close()
+    newLoc = input("What is the new location of the hive ? ")
+    hiveInfos[0] = "*"+newLoc + "\n"
+    
+    fileFlux = open("./Hives/"+hiveName, "w")
+    fileFlux.write("".join(hiveInfos))
+    fileFlux.close()
+    
+def hk_daughterModif(hiveName):
+    """ Fucntion that modifies the list of daughters of a specified hive
+    """
+    fileFlux = open("./Hives/"+hiveName, "r")
+    hiveInfos = fileFlux.readlines()
+    fileFlux.close()
+    action = int(input("Do you want to modify the name of a daughter (1) or add a daughter (2) ? "))
+    if action == 1:
+        action
+    
+    
+    
+    
